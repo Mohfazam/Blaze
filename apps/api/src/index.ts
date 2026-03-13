97dab351b4c136acf143f72d4b548a695955ec04
Mohfazam <mohd.sarwar.code@gmail.com>
2026-03-14 02:10:36 +0530

    DB

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import {hello} from "@blaze/types"
 
 dotenv.config()
 
@@ -9,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get("/health", (_, res) => {
-  res.json({ status: "ok" })
+  res.json({ status: "ok", message: hello })
 })
 
 const PORT = process.env.PORT || 4000
